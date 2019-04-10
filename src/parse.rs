07a89127db3e6c045356b8abeca4f9c6585bc638
Andreas Fuchs <asf@boinkor.net>
2019-04-10 11:55:09 +0200

    Revert all that error kind stuff and make it work

--- src/parse.rs
+++ src/parse.rs
@@ -36,7 +36,7 @@ impl From<u32> for ParseErrorKind {
 type Span<'a> = LocatedSpan<Input<'a>>;
 
 /// Indicates nom failed to parse a .htaccess file.
-pub type ParseError<'a> = nom::Err<Span<'a>, ParseErrorKind>;
+pub type ParseError<'a> = nom::Err<Span<'a>>;
 
 struct PWToken<'a> {
     position: Span<'a>,
@@ -48,60 +48,53 @@ struct UserToken<'a> {
     username: String,
 }
 
-named!(bcrypt_pw<Span, PWToken, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::Bcrypt),
-                     do_parse!(position: position!() >>
-                               peek!(alt_complete!(tag!("$2a$") | tag!("$2y$") | tag!("$2b$"))) >>
-                               pw: not_line_ending >>
-                               (PWToken{position, hash: PasswordHash::Bcrypt(pw.to_string()),})
-                     ))
+named!(bcrypt_pw<Span, PWToken>,
+       do_parse!(position: position!() >>
+                              peek!(alt_complete!(tag!("$2a$") | tag!("$2y$") | tag!("$2b$"))) >>
+                              pw: not_line_ending >>
+                              (PWToken{position, hash: PasswordHash::Bcrypt(pw.to_string()),})
+       )
 );
 
-named!(sha1_pw<Span, PWToken, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::SHA1),
-                     do_parse!(position: position!() >>
-                               tag!("{SHA}") >>
-                               pw: not_line_ending >>
-                               (PWToken{position, hash: PasswordHash::SHA1(pw.to_string())})))
+named!(sha1_pw<Span, PWToken>,
+       do_parse!(position: position!() >>
+                              tag!("{SHA}") >>
+                              pw: not_line_ending >>
+                              (PWToken{position, hash: PasswordHash::SHA1(pw.to_string())}))
 );
 
-named!(md5_pw<Span, PWToken, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::MD5),
-                     do_parse!(position: position!() >>
-                               tag!("$apr1$") >>
-                               pw: not_line_ending >>
-                               (PWToken{position, hash: PasswordHash::MD5(pw.to_string())})))
+named!(md5_pw<Span, PWToken>,
+       do_parse!(position: position!() >>
+                              tag!("$apr1$") >>
+                              pw: not_line_ending >>
+                              (PWToken{position, hash: PasswordHash::MD5(pw.to_string())}))
 );
 
-named!(crypt_pw<Span, PWToken, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::Crypt),
-                     do_parse!(position: position!() >>
-                               pw: not_line_ending >>
-                               (PWToken{position, hash: PasswordHash::Crypt(pw.to_string())})))
+named!(crypt_pw<Span, PWToken>,
+       do_parse!(position: position!() >>
+                              pw: not_line_ending >>
+                              (PWToken{position, hash: PasswordHash::Crypt(pw.to_string())}))
 );
 
-named!(password<Span, PWToken, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::BadPassword),
-                     alt!(bcrypt_pw | sha1_pw | md5_pw | crypt_pw)));
+named!(password<Span, PWToken>,
+       alt!(bcrypt_pw | sha1_pw | md5_pw | crypt_pw));
 
-named!(user<Span, UserToken, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::BadUsername),
-                     do_parse!(position: position!() >>
-                               user: terminated!(is_not!(":"), tag!(":")) >>
-                               (UserToken{position, username: user.fragment.to_string()}))));
+named!(user<Span, UserToken>,
+       do_parse!(position: position!() >>
+                              user: terminated!(is_not!(":"), tag!(":")) >>
+                              (UserToken{position, username: user.fragment.to_string()})));
 
 named!(
-    entry<Span, (UserToken, PWToken), ParseErrorKind>,
+    entry<Span, (UserToken, PWToken)>,
     do_parse!(user: user >>
               pw_hash: password >>
               ((user, pw_hash)))
 );
 
-named!(entries<Span, Vec<(UserToken, PWToken)>, ParseErrorKind>,
-       return_error!(ErrorKind::Custom(ParseErrorKind::BrokenHtpasswd),
-                     do_parse!(entries: terminated!(separated_list!(tag!("\n"), entry), opt!(line_ending)) >>
-                               add_return_error!(ErrorKind::Custom(ParseErrorKind::GarbageAtEnd), eof!()) >>
-                               (entries)))
+named!(entries<Span, Vec<(UserToken, PWToken)>>,
+       do_parse!(entries: terminated!(separated_list!(tag!("\n"), entry), opt!(line_ending)) >>
+                 eof!() >>
+                              (entries))
 );
 
 pub(crate) fn parse_entries(input: &str) -> Result<HashMap<String, PasswordHash>, ParseError> {
